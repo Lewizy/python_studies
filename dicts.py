79d651e2bb614f23aee2bc92cd141e4af51de17d
Lewis Betances <15459402+Lewizy@users.noreply.github.com>
2020-10-15 11:31:38 -0500

    dicts.py

--- dicts.py
+++ dicts.py
@@ -1,5 +1,6 @@
 
 
+
 ########################################dictionaries###################################################
 dict = {}
 dict ["State"] = "Alabama"
@@ -9,52 +10,64 @@ dict ["Color"] = "Brown"
 dict ["Religion"] = "Pizzianity"
 dict [5] = "Number five"
 
-print dict
-print dict.keys()
-print dict.values()
+
 
 dictionary = {}
 dictionary ["hi"] = "salute"
 print (dictionary)
 
-dictionaries_alternative = {
-	"name": "Jamaiquino",
-	"age" : "33",
-	"race":"Bomboclatian",
+dictionaries_person = {
+    "name":"Ballerino",
+    "nick name": "Jamaiquino",
+    "age" : "33",
+    "race":"Bomboclatian",
+    "favoriteFood": "lemons",
+    "favorite_tv_show": "the kardashians",
+    "petOwned": "cat",
+    "favorite tv line": "how bowt them grapples",
 }
 
-addingValuesTo_dictionaries = dictionaries_alternative["Hair color"] = "pink"
-print(dictionaries_alternative)
+the_popItem_method = dictionaries_person.popitem()
+addingValuesTo_dictionaries = dictionaries_person["hair color"] = "pink"
+#adding values
+the_pop_method = dictionaries_person.pop("petOwned")
+#removing values
 
-the_pop_method = dictionaries_alternative.pop("name")
+the_clear_method = dict.clear()
 
-print_dict_keys = dictionaries_alternative["age"]
+the_copy_method = dictionaries_person.copy() #copy dicts
+
+the_popItem_method = dictionaries_person.popitem()
+#removes last item, some versions only
+
+del dictionaries_person["favoriteFood"] #removes specified
+#del dictionaries_person  #deletes whole dicts
+
+print_dict_keys = dictionaries_person["age"]
 print(print_dict_keys)
 
-updating_values_inDict = dictionaries_alternative["race"] = "Celestial"
+updating_values_inDict = dictionaries_person["race"] = "celestial"
 print(updating_values_inDict+"<--updated")
 
-the_get_method = dictionaries_alternative.get("Hair color")
+the_get_method = dictionaries_person.get("Hair color")
 print(the_get_method)
 
-for print_all_keys in dictionaries_alternative:
+for print_all_keys in dictionaries_person:
     print(print_all_keys)
 
-for print_all_values in dictionaries_alternative:
-		print(dictionaries_alternative[print_all_values])	
+for print_all_values in dictionaries_person:
+        print(dictionaries_person[print_all_values])    
 
-for value_x in dictionaries_alternative.values():
-		print(value_x)
+for use_values_method in dictionaries_person.values():
+        print(use_values_method)
 
-for keys_items , values_items in dictionaries_alternative.items(): 
-		print(keys_items , values_items)
+for keys_items , values_items in dictionaries_person.items(): 
+        print(keys_items , values_items)
  
-if "age" in dictionaries_alternative:
-		print("age exist in dictionaries_alternative")
+if "age" in dictionaries_person:
+        print("age exist in dictionaries_person")
 
-the_length_method = len(dictionaries_alternative)
-print("The length is : --->>>", the_length_method)
+the_length_method = len(dictionaries_person)
+print("The length is-->>>>>>>>" , the_length_method) 
 
-
-
-
+print(the_copy_method)
