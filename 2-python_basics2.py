5126f67d7187de6e1b3eaf2be790d717770df62d
Lewis Betances <15459402+Lewizy@users.noreply.github.com>
2020-10-24 23:13:12 -0400

    note_worthies.py

--- 2-python_basics2.py
+++ 2-python_basics2.py
@@ -1,1 +1,21 @@
-print("testing")
+
+
+numTwoThree = "5"
+the_type_method = type(numTwoThree)
+#print(the_type_method)
+
+the_power_10th_on_35 = 35e5
+print(the_power_10th_on_35) #3500000.0
+
+range_method = range(0,4,2)
+for range_i in range_method:
+		print(range_i)
+
+range_method2 = range(0,6);
+range_method3 = range(7)
+
+import random
+print(random.randrange(0,90))
+
+str_some = "mississipianopollis"
+print(str_some[-8:-2]) #nopoll
