bbbed1a621e729fac2cac6dea0d7bb157c88dacb
Lewis Betances <15459402+Lewizy@users.noreply.github.com>
2020-10-26 23:50:19 -0400

    strings.py

--- strings.py
+++ strings.py
@@ -9,13 +9,22 @@ print("length is =", len(multiLine_str_tripleQuotation))
 print("one line", "two line", sep="-----")  #one line-----two line
 
 stripMethod = "   hello   "
-print(stripMethod.strip())  #removes spaces
+print(stripMethod.strip())#removes spaces
 
 lowerMethod = "hELLo UnIvERse"
-print(lowerMethod.lower())  #returns lower cases
+print(lowerMethod.lower())#returns lower cases
 
 upperMethod = "these are capital letters"
-print(upperMethod.upper())
+print(upperMethod.upper())#returns capitals
 
 replaceMethod = "Replace me with yo"
 print(replaceMethod.replace("me","yo"))
+
+splitMethod = "hello - universe"
+print(splitMethod.split(","))
+#method splits the string into subtstrings if it 
+#finds instances of the separator
+
+checkIfTxt = 'Check if the wapa is in text'
+checkFunc = "wapa" in checkIfTxt
+print(checkFunc)
