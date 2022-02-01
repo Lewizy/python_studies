787332a9cdabf5951b3b65d67ffc50510741ccbf
laptop <lewizyx@gmail.com>
2022-01-31 21:47:04 -0500

    started commits

--- json_file.py
+++ json_file.py
@@ -15,3 +15,21 @@ print(type(json.dumps(True)))
 print(type(json.dumps(-31.25)))
 print(type(json.dumps(False)))
 print(type(json.dumps([26, 56, 533, -13.32])))
+
+person_1 = {
+  "name": "John",
+  "age": 30,
+  "married": True,
+  "divorced": False,
+  "children": ("Ann", "Billy"),
+  "pets": None,
+  "cars": [
+    {"model": "BMW 230", "mpg": 27.5},
+    {"model": "Ford Edge", "mpg": 24.1}
+  ]
+}
+# use indent to read better
+print(json.dumps(person_1, indent=1))
+
+# use . and a space to separate objects, and a space, a = and a space to separate keys from their values:
+print(json.dumps(person_1, indent=1, separators=(".", "=")))
--- note_worthies.py
+++ note_worthies.py
@@ -1,4 +1,8 @@
+import random
+print(random.randrange(0, 90))
 
+str_some = "mississipianopollis"
+print(str_some[-8:-2]) #nopoll
 
 numTwoThree = "5"
 the_type_method = type(numTwoThree)
@@ -8,15 +12,13 @@ the_power_10th_on_35 = 35e5
 print(the_power_10th_on_35) #3500000.0
 
 range_method = range(0,4,2)
+
 for range_i in range_method:
    print(range_i)
+
 
 range_method2 = range(0,6);
 range_method3 = range(7)
 
-import random
-print(random.randrange(0,90))
 
-str_some = "mississipianopollis"
-print(str_some[-8:-2]) #nopoll
   
